dd0de029117b4f4ae41ef9fc1519e62edbe6e346
szt <1435987729@qq.com>
2022-04-12 10:12:33 +0800

    model without discourse relation

--- modules/Discourse.py
+++ modules/Discourse.py
@@ -17,11 +17,6 @@ class Discourse(nn.Module):
         self.graph_conv1 = TransformerConv(in_channels=self.config.bert_dims, out_channels=self.conv1_dim//self.config.graph_conv_head, heads=self.config.graph_conv_head, edge_dim=self.conv1_dim)
         self.graph_conv2 = TransformerConv(in_channels=self.conv1_dim, out_channels=self.conv2_dim//self.config.graph_conv_head, heads=self.config.graph_conv_head , edge_dim=self.conv2_dim)
         
-        self.relation_embedding1 = nn.Linear(1, self.conv1_dim)
-        self.relation_embedding2 = nn.Linear(1, self.conv2_dim)
-        self.is_main_embedding1 = nn.Linear(1, self.conv1_dim)
-        self.is_main_embedding2 = nn.Linear(1, self.conv2_dim)
-        
         self.sentence_fc = nn.Linear(self.conv2_dim, len(vocab._id2label))
         self.edu_fc = nn.Linear(self.conv2_dim, len(vocab._id2label))
 
@@ -46,8 +41,8 @@ class Discourse(nn.Module):
         edu_embed = F.dropout(edu_hidden, p=self.config.dropout_emb, training=self.training)
         
         node_hidden = edu_embed.reshape(-1, edu_embed.shape[-1])
-        node_hidden = self.graph_conv1(x=node_hidden, edge_index=batch_edge, edge_attr=self.relation_embedding1(batch_edge_type)+self.is_main_embedding1(batch_is_main))
-        node_hidden = self.graph_conv2(x=node_hidden, edge_index=batch_edge, edge_attr=self.relation_embedding2(batch_edge_type)+self.is_main_embedding2(batch_is_main))
+        node_hidden = self.graph_conv1(x=node_hidden, edge_index=batch_edge)
+        node_hidden = self.graph_conv2(x=node_hidden, edge_index=batch_edge)
         
         last_index = (edu_lengths-1).unsqueeze(1).unsqueeze(2)
         last_index = last_index.repeat([1, 1, self.conv2_dim])
